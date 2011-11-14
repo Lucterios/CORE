d1662323b506239c528b465e9751b32d1deda30c
user <user@69e63612-969a-4371-9866-b14aae95a0cb>
2011-11-14 02:03:13 +0000

    Modifications module CORE (1.3.1.680)
    git-svn-id: http://projets.lucterios.org/svn/CORE@1448 69e63612-969a-4371-9866-b14aae95a0cb

--- CORE/setup.inc.php
+++ CORE/setup.inc.php
@@ -18,7 +18,7 @@
 // 
 // 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
 // setup file write by SDK tool
-// --- Last modification: Date 11 November 2011 19:01:59 By  ---
+// --- Last modification: Date 13 November 2011 19:09:27 By  ---
 
 $extention_name="CORE";
 $extention_description="Coeur du serveur Lucterios.{[newline]}Gère la connexion au logiciel, les droits d`accès ainsi que l`integration des autres modules.";
@@ -30,7 +30,7 @@ $extension_libre=true;
 $version_max=1;
 $version_min=3;
 $version_release=1;
-$version_build=679;
+$version_build=680;
 
 $depencies=array();
 
--- CORE/xfer_custom.inc.php
+++ CORE/xfer_custom.inc.php
@@ -1,24 +1,24 @@
 <?php
+// 	This file is part of Diacamma, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
+// 	Thanks to have payed a retribution for using this module.
 // 
-//     This file is part of Lucterios.
+// 	Diacamma is free software; you can redistribute it and/or modify
+// 	it under the terms of the GNU General Public License as published by
+// 	the Free Software Foundation; either version 2 of the License, or
+// 	(at your option) any later version.
 // 
-//     Lucterios is free software; you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation; either version 2 of the License, or
-//     (at your option) any later version.
+// 	Diacamma is distributed in the hope that it will be useful,
+// 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 	GNU General Public License for more details.
 // 
-//     Lucterios is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
+// 	You should have received a copy of the GNU General Public License
+// 	along with Lucterios; if not, write to the Free Software
+// 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // 
-//     You should have received a copy of the GNU General Public License
-//     along with Lucterios; if not, write to the Free Software
-//     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-// 
-// 	Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
-//  // library file write by SDK tool
-// --- Last modification: Date 21 July 2010 9:10:59 By  ---
+// 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
+// library file write by SDK tool
+// --- Last modification: Date 13 November 2011 19:09:27 By  ---
 
 //@BEGIN@
 /**
@@ -350,6 +350,7 @@ if ((type=='list') || (type=='listmult')) {
 		else if (isset($this->m_context['ACT'])) {
 			unset($currentCriteria[$this->m_context['ACT']]);
 		}
+		unset($this->m_context['ACT']);
 	}
 
 	/**
@@ -439,6 +440,7 @@ if ((type=='list') || (type=='listmult')) {
 			$label->setLocation($posX,$posY,4);
 		}
 		$this->addComponent($label);
+		$posY++;
 		foreach($criteriaDesc as $id=>$criteriaText) {
 			$label = new Xfer_Comp_LabelForm('labelSearchText_'.$id);
 			$label->setValue($criteriaText);
