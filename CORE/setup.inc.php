4f30e1c92c899e8ef7b22cbb8bf699f7531767c6
user <user@69e63612-969a-4371-9866-b14aae95a0cb>
2011-11-29 02:03:30 +0000

    Modifications module CORE (1.3.1.721)
    git-svn-id: http://projets.lucterios.org/svn/CORE@1471 69e63612-969a-4371-9866-b14aae95a0cb

--- CORE/setup.inc.php
+++ CORE/setup.inc.php
@@ -18,7 +18,7 @@
 // 
 // 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
 // setup file write by SDK tool
-// --- Last modification: Date 22 November 2011 21:58:13 By  ---
+// --- Last modification: Date 29 November 2011 0:18:44 By  ---
 
 $extention_name="CORE";
 $extention_description="Coeur du serveur Lucterios.{[newline]}Gère la connexion au logiciel, les droits d`accès ainsi que l`integration des autres modules.";
@@ -30,7 +30,7 @@ $extension_libre=true;
 $version_max=1;
 $version_min=3;
 $version_release=1;
-$version_build=720;
+$version_build=721;
 
 $depencies=array();
 
