bc6203d19ae21b97ce7c5dad3960639164d5118b
Laurent GAY <l.gay@sd-libre.fr>
2012-08-30 07:00:16 -0700

    Controle sur le model d'impression

--- CORE/ConvertPrintModel.inc.php
+++ CORE/ConvertPrintModel.inc.php
@@ -1,24 +1,21 @@
 <?php
-// 	This file is part of Diacamma, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
-// 	Thanks to have payed a retribution for using this module.
+// This file is part of Lucterios, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
+// Thanks to have payed a donation for using this module.
 // 
-// 	Diacamma is free software; you can redistribute it and/or modify
-// 	it under the terms of the GNU General Public License as published by
-// 	the Free Software Foundation; either version 2 of the License, or
-// 	(at your option) any later version.
+// Lucterios is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
 // 
-// 	Diacamma is distributed in the hope that it will be useful,
-// 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-// 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// 	GNU General Public License for more details.
+// Lucterios is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 // 
-// 	You should have received a copy of the GNU General Public License
-// 	along with Lucterios; if not, write to the Free Software
-// 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-// 
-// 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
-// library file write by SDK tool
-// --- Last modification: Date 15 November 2011 0:23:20 By  ---
+// You should have received a copy of the GNU General Public License
+// along with Lucterios; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+// library file write by Lucterios SDK tool
 
 //@BEGIN@
 class ModelConverter {
@@ -264,7 +261,7 @@ function checkDBModel($extension,$printmodel,$checkModify = false) {
 		list($id,$model,$modify) = getDBModel($extension,$printmodel);
 		if($id == 0) {
 			$MODEL_DEFAULT = '';
-			require_once$printfile;
+			require_once($printfile);
 			require_once("CORE/printmodel.tbl.php");
 			$model = new DBObj_CORE_printmodel;
 			$model->extensionid = $extension;
@@ -277,9 +274,9 @@ function checkDBModel($extension,$printmodel,$checkModify = false) {
 			list($id,$model) = getDBModel($extension,$printmodel);
 			$res = 'Ajouter';
 		}
-		else if(($modify == 'n') && $checkModify) {
+		else if(($modify == 'n') && ($checkModify || (trim($model->model)==''))) {
 			$MODEL_DEFAULT = '';
-			require_once$printfile;
+			require_once($printfile);
 			require_once("CORE/printmodel.tbl.php");
 			$model = new DBObj_CORE_printmodel;
 			$model->get($id);
@@ -291,6 +288,11 @@ function checkDBModel($extension,$printmodel,$checkModify = false) {
 			$res = 'Modifier';
 		}
 		else $res = "Rien";
+		if (trim($model->model)=='') {
+			logAutre(" *** Model ($extension::$printmodel) non chargé ($res) *** ");
+			require_once("CORE/Lucterios_Error.inc.php");
+			throw new LucteriosException(GRAVE,"Problème de model d'impression!");
+		}
 	}
 	return array($id,$model,$res);
 }
