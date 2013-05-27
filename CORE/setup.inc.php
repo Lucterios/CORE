70e4e0eedc73896100c38c33998f0d811c66847a
Builder <support@sd-libre.fr>
2013-05-27 08:29:14 +0200

    Pour validation

--- CORE/setup.inc.php
+++ CORE/setup.inc.php
@@ -1,13 +1,13 @@
 <?php
-// This file is part of Lucterios, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
-// Thanks to have payed a donation for using this module.
+// This file is part of Diacamma, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
+// Thanks to have payed a retribution for using this module.
 // 
-// Lucterios is free software; you can redistribute it and/or modify
+// Diacamma is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
 // 
-// Lucterios is distributed in the hope that it will be useful,
+// Diacamma is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Lucterios; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+// Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
 // setup file write by Lucterios SDK tool
 
 $extention_name="CORE";
@@ -27,7 +29,7 @@ $extension_libre=true;
 $version_max=1;
 $version_min=5;
 $version_release=3;
-$version_build=860;
+$version_build=861;
 
 $depencies=array();
 
