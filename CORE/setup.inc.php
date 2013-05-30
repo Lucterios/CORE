a1c287a860de09ff6bc26fa5e341f033ff48e3f4
Laurent GAY <l.gay@sd-libre.fr>
2013-05-30 21:23:39 +0200

    Controle 'gc_enable' pour PHP < 5.3

--- CORE/setup.inc.php
+++ CORE/setup.inc.php
@@ -1,13 +1,13 @@
 <?php
-// This file is part of Diacamma, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
-// Thanks to have payed a retribution for using this module.
+// This file is part of Lucterios/Diacamma, a software developped by 'Le Sanglier du Libre' (http://www.sd-libre.fr)
+// thanks to have payed a retribution for using this module.
 // 
-// Diacamma is free software; you can redistribute it and/or modify
+// Lucterios/Diacamma is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
 // 
-// Diacamma is distributed in the hope that it will be useful,
+// Lucterios/Diacamma is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
@@ -15,8 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Lucterios; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-// 
-// Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
 // setup file write by Lucterios SDK tool
 
 $extention_name="CORE";
@@ -29,7 +27,7 @@ $extension_libre=true;
 $version_max=1;
 $version_min=5;
 $version_release=3;
-$version_build=861;
+$version_build=862;
 
 $depencies=array();
 
