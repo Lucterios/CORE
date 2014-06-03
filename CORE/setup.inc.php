c8db1e2804a5f544615f5319968928a56b115f9c
support <support@sd-libre.fr>
2014-06-03 09:33:31 +0000

    CORE 1.7

--- CORE/setup.inc.php
+++ CORE/setup.inc.php
@@ -1,20 +1,4 @@
 <?php
-// This file is part of Lucterios/Diacamma, a software developped by 'Le Sanglier du Libre' (http://www.sd-libre.fr)
-// thanks to have payed a retribution for using this module.
-// 
-// Lucterios/Diacamma is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-// 
-// Lucterios/Diacamma is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with Lucterios; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // setup file write by Lucterios SDK tool
 
 $extention_name="CORE";
@@ -25,8 +9,8 @@ $extention_titre="Noyau Lucterios";
 $extension_libre=true;
 
 $version_max=1;
-$version_min=5;
-$version_release=7;
+$version_min=6;
+$version_release=1;
 $version_build=880;
 
 $depencies=array();
