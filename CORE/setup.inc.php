5c16b09a93a691459270040e750be27cd09656e3
support <support@sd-libre.fr>
2014-08-19 17:50:14 +0000

    Impression pdf

--- CORE/setup.inc.php
+++ CORE/setup.inc.php
@@ -1,4 +1,20 @@
 <?php
+// This file is part of Lucterios/Diacamma, a software developped by 'Le Sanglier du Libre' (http://www.sd-libre.fr)
+// thanks to have payed a retribution for using this module.
+// 
+// Lucterios/Diacamma is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// Lucterios/Diacamma is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Lucterios; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // setup file write by Lucterios SDK tool
 
 $extention_name="CORE";
@@ -11,7 +27,7 @@ $extension_libre=true;
 $version_max=1;
 $version_min=6;
 $version_release=1;
-$version_build=880;
+$version_build=882;
 
 $depencies=array();
 
--- CORE/xlp2pdf.inc.php
+++ CORE/xlp2pdf.inc.php
@@ -1,5 +1,23 @@
 <?php
+// This file is part of Lucterios/Diacamma, a software developped by 'Le Sanglier du Libre' (http://www.sd-libre.fr)
+// thanks to have payed a retribution for using this module.
+// 
+// Lucterios/Diacamma is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// Lucterios/Diacamma is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Lucterios; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+// library file write by Lucterios SDK tool
 
+//@BEGIN@
 require_once('CORE/fpdf.php');
 require_once('CORE/XMLparse.inc.php');
 
@@ -13,9 +31,9 @@ $LIST_COLOR=array(
 	'purple'=>array(255,0,255),
 	'grey'=>array(192,192,192),
 	'white'=>array(255,255,255)
-);	
+);
 
-class TextFormated 
+class TextFormated
 {
 	private $nb_line=0;
 	private $content=array(array());
@@ -27,7 +45,7 @@ class TextFormated
 	private $size;
 	private $intersize;
 	private $color;
-	
+
 	public $sesure_chars=array(' ','-','/');
 
 	private function _addtext($text) {
@@ -43,7 +61,7 @@ class TextFormated
 		}
 		$this->content[$this->nb_line][]=array($text,$style,$this->fontname,$this->size,$this->intersize,$this->color);
 	}
-	
+
 	private function _sub_text($xmlitem) {
 		$tagname = strtolower($xmlitem->getTagName());
 		if ($tagname=='br') {
@@ -64,15 +82,16 @@ class TextFormated
 			if ($xmlitem->getAttributeValue('color')!==false)
 				$this->color=$xmlitem->getAttributeValue('color');
 		}
-		for($idx=0;$idx<=count($xmlitem->getChilds());$idx++) {
+		$child_list=$xmlitem->getChilds();
+		for($idx=0;$idx<=count($child_list);$idx++) {
 			if (array_key_exists($idx,$xmlitem->cdatas)) {
 				$text=trim($xmlitem->cdatas[$idx]);
 				if ($text!='') {
 					$this->_addtext($text);
 				}
 			}
-			if (isset($xmlitem->getChilds()[$idx])) {
-				$this->_sub_text($xmlitem->getChilds()[$idx]);
+			if (isset($child_list[$idx])) {
+				$this->_sub_text($child_list[$idx]);
 			}
 		}
 		$this->B=$last_B;
@@ -80,7 +99,7 @@ class TextFormated
 		$this->U=$last_U;
 		$this->color=$last_color;
 	}
-		
+
 	function TextFormated($xmltest,$fontname,$size,$intersize) {
 		$this->B = 0;
 		$this->I = 0;
@@ -91,7 +110,7 @@ class TextFormated
 		$this->intersize=$intersize;
    		$this->_sub_text($xmltest);
 	}
-	
+
 	function _getNewTextlines($fpdf,$width) {
 		$new_content=array();
 		$width_lines=array();
@@ -132,8 +151,8 @@ class TextFormated
 								unset($new_line[$otherid]);
 							$this->content[$line_idx][$item_idx][0]=substr($text_current,$index+1);
 							for($otherid=0;$otherid<$item_idx;$otherid++)
-								unset($this->content[$line_idx][$item_idx]);							
-							$item_w=$fpdf->GetStringWidth($new_line[$item_idx][0]);							
+								unset($this->content[$line_idx][$item_idx]);
+							$item_w=$fpdf->GetStringWidth($new_line[$item_idx][0]);
 							$new_content[]=$new_line;
 							$width_lines[]=$line_w+$item_w;
 						}
@@ -155,7 +174,7 @@ class TextFormated
 		}
 		return array($new_content,$width_lines);
 	}
-	
+
 	function fillPDF($fpdf,$posX,$width,$align,$simpleText=True) {
 		global $LIST_COLOR;
 		list($new_content,$width_lines) = $this->_getNewTextlines($fpdf,$width);
@@ -168,7 +187,7 @@ class TextFormated
 			if ($line_idx>0)
 				$fpdf->Ln($last_itersize);
 			$line_w=$width_lines[$line_idx];
-			if (($align=='left') || ($align=='start'))				
+			if (($align=='left') || ($align=='start'))
 				$currentX=$posX;
 			else if ($align=='center')
 				$currentX=$posX+($width-$line_w)/2;
@@ -193,7 +212,7 @@ class TextFormated
 			if (!$simpleText && ($line_idx>0))
 				$interspace+=$last_itersize;
 			$line_idx++;
-		}		
+		}
 		if ($simpleText && (abs($min_y-$max_y)<0.001)) {
 			$min_y=$fpdf->y-($this->size/$fpdf->k)/2;
 			$max_y=$fpdf->y+($this->size/$fpdf->k)/2;
@@ -206,9 +225,9 @@ class TextFormated
 class LucteriosPDF extends FPDF
 {
 	private $_xml;
-	
+
 	private $y_offset;
-	
+
 	private function _init() {
 		$this->AddFont('sans-serif','','FreeSans.php');
 		$this->AddFont('sans-serif','B','FreeSansB.php');
@@ -233,17 +252,18 @@ class LucteriosPDF extends FPDF
 		$this->tMargin = (float)$model->getAttributeValue('margin_top');
 		$this->rMargin = (float)$model->getAttributeValue('margin_right');
 
-		$header=$model->getChildsByTagName('page')[0]->getChildsByTagName('header')[0];
-		$this->header_h=(float)$header->getAttributeValue('extent');
-		$bottom=$model->getChildsByTagName('page')[0]->getChildsByTagName('bottom')[0];
-		$this->bottom_h=(float)$bottom->getAttributeValue('extent');
-		
+		$page=$model->getChildsByTagName('page');
+		$header=$page[0]->getChildsByTagName('header');
+		$this->header_h=(float)$header[0]->getAttributeValue('extent');
+		$bottom=$page[0]->getChildsByTagName('bottom');
+		$this->bottom_h=(float)$bottom[0]->getAttributeValue('extent');
+
 		$bo_m=(float)$model->getAttributeValue('margin_bottom');
 		$this->SetAutoPageBreak(true,$this->bottom_h+$bo_m);
 		$this->SetFont('Helvetica','',12);
 
 	}
-	
+
 	private function getTopComponent($xmlitem) {
 		$currentY=$this->y_offset+(float)$xmlitem->getAttributeValue('top');
 		$spacing=(float)$xmlitem->getAttributeValue('spacing');
@@ -255,17 +275,17 @@ class LucteriosPDF extends FPDF
 		}
 		return $currentY;
 	}
-		
+
 	private function parse_text($xmltext) {
 		$currentY=$this->getTopComponent($xmltext);
 		$txt = new TextFormated($xmltext,$xmltext->getAttributeValue('font_family'),
 			$xmltext->getAttributeValue('font_size'), $xmltext->getAttributeValue('line_height'));
-		$this->SetY($currentY);		
+		$this->SetY($currentY);
 		list($min_y,$max_y,$interspace)=$txt->fillPDF($this,$this->lMargin+(float)$xmltext->getAttributeValue('left'),
 			(float)$xmltext->getAttributeValue('width'),$xmltext->getAttributeValue('text_align'));
 		$this->Ln(max($interspace,$max_y-$min_y));
 	}
-	
+
 	private function parse_image($xmlimage,$posY=0,$posX=0,$weight=0) {
 		if ($xmlimage->getAttributeValue('top')!==false)
 			$currentY=$this->getTopComponent($xmlimage);
@@ -292,7 +312,7 @@ class LucteriosPDF extends FPDF
 		}
 		else {
 			$img_file=$img_content;
-		}			
+		}
 		$type=image_type_to_extension(exif_imagetype($img_file));
 		$type=str_replace('.','',$type);
 		$this->setY($currentY);
@@ -320,12 +340,12 @@ class LucteriosPDF extends FPDF
 			else {
 				$txt = new TextFormated($cell,$cell->getAttributeValue('font_family'),
 					$cell->getAttributeValue('font_size')-(($lineTop<0)?0.3:0),$cell->getAttributeValue('line_height'));
-				$this->SetY($posY+2.5);		
+				$this->SetY($posY+2.5);
 				$txt->sesure_chars=array(' ','/');
 				list($min_y,$max_y,$interspace)=$txt->fillPDF($this,$posX+$col_x-0.2,
 					$width_column-1.2,$cell->getAttributeValue('text_align'),False);
 			}
-			$col_x+=$width_column;			
+			$col_x+=$width_column;
 			$col_min_y=min($col_min_y,$min_y-0.5);
 			$col_max_y=max($col_max_y,$max_y+0.5);
 			$col_interspace=max($col_interspace,$interspace+0.5);
@@ -338,7 +358,7 @@ class LucteriosPDF extends FPDF
 		}
 		$this->Line($posX, $col_min_y, $posX, $col_max_y);
 		foreach($width_columns as $width_column) {
-			$col_x+=$width_column;			
+			$col_x+=$width_column;
 			$this->Line($posX+$col_x, $col_min_y, $posX+$col_x, $col_max_y);
 		}
 		if ($lineTop<0)
@@ -356,7 +376,8 @@ class LucteriosPDF extends FPDF
 		$cellcolumns=array();
 		foreach($xmlcolumns as $xmlcolumn) {
 			$width_columns[]=(float)$xmlcolumn->getAttributeValue('width');
-			$cellcolumns[]=$xmlcolumn->getChildsByTagName('cell')[0];
+			$cells=$xmlcolumn->getChildsByTagName('cell');
+			$cellcolumns[]=$cells[0];
 		}
 		list($last_top,$last_bottom)=$this->_addTableLine($cellcolumns,$width_columns,$currentX+$this->lMargin,$currentY,-1);
 		$rows=$xmltable->getChildsByTagName('rows');
@@ -372,7 +393,7 @@ class LucteriosPDF extends FPDF
 			list($last_top,$last_bottom)=$this->_addTableLine($cells,$width_columns,$currentX+$this->lMargin,$this->y,$last_bottom);
 		}
 	}
-	
+
 	private function _parse_comp($comp,$y_offset) {
 		$last_y_offset=$this->y_offset;
 		$this->y_offset=$y_offset;
@@ -387,15 +408,17 @@ class LucteriosPDF extends FPDF
 		}
 		$this->y_offset=$last_y_offset;
 	}
-	
+
 	public function Header() {
-		$header=$this->xml->getResult()->getChildsByTagName('page')[0]->getChildsByTagName('header')[0];
-		$this->_parse_comp($header,$this->tMargin);
+		$page=$this->xml->getResult()->getChildsByTagName('page');
+		$header=$page[0]->getChildsByTagName('header');
+		$this->_parse_comp($header[0],$this->tMargin);
 	}
 
 	public function Footer() {
-		$bottom=$this->xml->getResult()->getChildsByTagName('page')[0]->getChildsByTagName('bottom')[0];
-		$this->_parse_comp($bottom,-1*$this->bMargin);		
+		$page=$this->xml->getResult()->getChildsByTagName('page');
+		$bottom=$page[0]->getChildsByTagName('bottom');
+		$this->_parse_comp($bottom[0],-1*$this->bMargin);
 	}
 
 	public function Execute($xml_content) {
@@ -403,7 +426,8 @@ class LucteriosPDF extends FPDF
 		$this->xml->setInputString($xml_content);
 		$this->xml->parse();
 		$this->_init();
-		$bodies=$this->xml->getResult()->getChildsByTagName('page')[0]->getChildsByTagName('body');
+		$page=$this->xml->getResult()->getChildsByTagName('page');
+		$bodies=$page[0]->getChildsByTagName('body');
 		foreach($bodies as $body) {
 			$this->AddPage();
 			$this->_parse_comp($body,$this->header_h+$this->tMargin);
@@ -420,9 +444,9 @@ function transforme_xml2pdf($xml_content) {
 function transform_file_xml2pdf($xml_file,$pdf_file) {
 	$pdf_content = transforme_xml2pdf(file_get_contents($xml_file));
 	@unlink($pdf_file);
-	if ($pdf_content != '') 
+	if ($pdf_content != '')
 		file_put_contents($pdf_file, $pdf_content);
 	return is_file($pdf_file);
 }
-
+//@END@
 ?>
